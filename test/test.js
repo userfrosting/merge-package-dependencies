a0b8cabbc122f41d0f98118743ad64842c83d116
Jordan Mele <jordan.mele@outlook.com.au>
2017-07-22 17:00:08 +1000

    Unit tests for `yarn` and logging improvements. Resolves #2

--- test/test.js
+++ test/test.js
@@ -68,6 +68,72 @@ describe("#npmMerge", function() {
     /** @todo Conflict in dependencies */
 });
 
+describe("#yarnMerge", function() {
+
+    it('should throw exception when provided with an invalid package template.', function() {
+        let template = JSON.parse(fs.readFileSync('./test/resources/yarn/templates/templateInvalid.json'));
+        let packagePaths = [
+            './test/resources/yarn/packages/',
+            './test/resources/yarn/packages/package2',
+            './test/resources/yarn/packages/package3.json'
+        ];
+        assert.throws(() => { packageMerge.yarn(template, packagePaths) }, packageMerge.exceptions.InvalidNpmPackageException);
+    });
+
+    it('should throw exception when provided with a path to an invalid package', function() {
+        let template = JSON.parse(fs.readFileSync('./test/resources/yarn/templates/template.json'));
+        let packagePaths = [
+            './test/resources/yarn/packages/',
+            './test/resources/yarn/packages/package2',
+            './test/resources/yarn/packages/package3.json',
+            './test/resources/yarn/packages/packageInvalid'
+        ];
+        assert.throws(() => { packageMerge.yarn(template, packagePaths) }, packageMerge.exceptions.InvalidNpmPackageException);
+    });
+
+    it('should return expected data when merging 3 packages', function() {
+        let template = JSON.parse(fs.readFileSync('./test/resources/yarn/templates/template.json'));
+        let packagePaths = [
+            './test/resources/yarn/packages/',
+            './test/resources/yarn/packages/package2',
+            './test/resources/yarn/packages/package3.json'
+        ];
+        let expectedResult = require("./resources/yarn/expectedResults/pkg1.json");
+        let result = packageMerge.yarn(template, packagePaths);
+        assert.deepEqual(result, expectedResult);
+    });
+
+    it('should return expected data when merging 1 package', function() {
+        let template = JSON.parse(fs.readFileSync('./test/resources/yarn/templates/template.json'));
+        let packagePaths = [
+            './test/resources/yarn/packages/'
+        ];
+        let expectedResult = JSON.parse(fs.readFileSync("./test/resources/yarn/expectedResults/pkg2"));
+        let result = packageMerge.yarn(template, packagePaths);
+        assert.deepEqual(result, expectedResult);
+    });
+
+    it('should save generated package to specified destination', function() {
+        let template = JSON.parse(fs.readFileSync('./test/resources/yarn/templates/template.json'));
+        let packagePaths = [
+            './test/resources/yarn/packages/',
+            './test/resources/yarn/packages/package2',
+            './test/resources/yarn/packages/package3.json'
+        ];
+        let expectedResult = JSON.parse(fs.readFileSync("./test/resources/yarn/expectedResults/pkg1.json"));
+        packageMerge.yarn(template, packagePaths, "./test/working/yarn/");
+        let result = JSON.parse(fs.readFileSync("./test/working/yarn/package.json"));
+        assert.deepEqual(result, expectedResult);
+
+        // Cleaning time!
+        fs.unlinkSync("./test/working/yarn/package.json");
+    });
+
+    /** @todo Somehow figure out a way to test console output */
+
+    /** @todo Conflict in dependencies */
+});
+
 describe("#bowerMerge", function() {
 
     it('should throw exception when provided with an invalid package template.', function() {
